472eb3bbbf3fd2b6a92f36f497505ade36fab697
yahdielo <yahdielaz@gmail.com>
2023-01-07 23:32:15 +0000

    binare tree insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,21 @@
+#include "binary_tress.h"
+/**
+ * binary_tree_insert_left - insert a new node to the left of parent
+ * @parent: parent node
+ * @value: value to store in newnode
+ * Return: newnode
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+
+	parent->left = newnode;
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
