8db7b80e3cc50d1d43d52ab409997c8463681921
yahdielo <yahdielaz@gmail.com>
2023-01-07 22:58:00 +0000

    binare tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,7 +22,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if(parent == NULL)
 		return (NULL);
 
-	binary_tree_t *root = create(value);
+	binary_tree_t *root;
+	root = create(value);
 
 	root->parent = parent;
 
