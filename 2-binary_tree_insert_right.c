a48e7b0410427c68f6d21355e1394a6e4eb4cd71
yahdielo <yahdielaz@gmail.com>
2023-01-08 04:17:06 +0000

    fixed typo

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,8 +15,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newnode->parent = parent;
-	newnode->left = NULL;
 	newnode->n = value;
+	newnode->left = NULL;
 
 	if (parent->right != NULL)
 	{
