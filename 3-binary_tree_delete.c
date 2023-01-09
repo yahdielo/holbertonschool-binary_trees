7c0b13bec356f3cab1114ef90a347e9c558789e4
yahdielo <yahdielaz@gmail.com>
2023-01-09 14:24:01 +0000

    delet everything in the binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delet entyre tree
+ * @tree: pointer to root of binary tree
+ * i
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return;
+
+	binary_tree_delet(tree->left);
+	binary_tree_delet(tree->right);
+
+	free(tree);
+}
