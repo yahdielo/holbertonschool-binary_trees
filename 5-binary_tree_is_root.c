a8f14b53a975531322fa14fdced165b85fbcf2d9
yahdielo <yahdielaz@gmail.com>
2023-01-11 03:28:40 +0000

    find if node is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_is_root - find if node is root
+ * @node: node to check if is root
+ * Return: always 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
