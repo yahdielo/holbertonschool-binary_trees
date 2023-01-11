d4074d270b79067a0eb7d8162052f13b940f1390
yahdielo <yahdielaz@gmail.com>
2023-01-11 05:32:09 +0000

    print tree inorder

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_tree.h"
+/**
+ * binary_tree_inorder - print binary tree in order
+ * @tree: pointer to tree root
+ * @func: fucntion to print tree element
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->n, func);
+	func(tree->n);
+	binary_tree_inorder(tree->n, func);
+}
