31654c062cc0ba84ced0b24f2cfc0816a09790a6
yahdielo <yahdielaz@gmail.com>
2023-01-11 17:28:02 +0000

    print size

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,15 +4,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t size_right, size_left;
+	size_t size_right, size_left, depth;
 
 	if (tree == NULL)
 		return (0);
-	else
+
+	if (!tree->left && !tree->right)
 	{
-	size_left = binary_tree_height(tree->left) ;
-	size_right = binary_tree_height(tree->right);
+		depth = 0;
+		return (depth);
 	}
+
+	size_left = binary_tree_height(tree->left);
+	size_right = binary_tree_height(tree->right);
 
 	if (size_left > size_right)
 		return (size_left + 1);
