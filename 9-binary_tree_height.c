123a3b417fab9ae27e8a249a9cf01316eef3b2c1
yahdielo <yahdielaz@gmail.com>
2023-01-11 17:35:33 +0000

    print size

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,8 @@
 #include "binary_trees.h"
 /**
- *
+ * binary_tree_height - get depth of binary tree
+ * @tree: pointer to root of binary tree
+ * Return: heigth of tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
