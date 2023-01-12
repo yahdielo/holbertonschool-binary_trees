fb9f28ca829b53148c14ea267afd018a77b687e0
yahdielo <yahdielaz@gmail.com>
2023-01-12 19:53:47 +0000

    count nodes with atleast one child

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,7 +14,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left || tree->right)
 	{
 		count = binary_tree_nodes(tree->left) + 1;
-		count = count +binary_tree_nodes(tree->right);
+		count = count + binary_tree_nodes(tree->right);
 	}
 	return (count);
 }
